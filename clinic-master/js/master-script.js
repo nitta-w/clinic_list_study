352ef9867911754ece953724c9a90f384b9fe839
nittta-w <w.nitta@support-inc.co.jp>
2026-07-04 11:50:39 +0900

    jsonからアコーディオンを生成

--- clinic-master/js/master-script.js
+++ clinic-master/js/master-script.js
@@ -1,18 +1,25 @@
 export async function createMaster(){
-	let clinicData = null;
+	let clinicDatas = [];
 	let mapSvgCode = '';
 
 	// クリニック情報を取得
 	try{
 		const response = await fetch('../clinic-master/js/clinic.json');
-		clinicData = await response.json();
+		if (!response.ok) {
+			throw new Error(`jsonが見つからないよ: ${response.status}`);
+		}
+		clinicDatas = await response.json();
 	}catch(e){
 		console.error(e);
 	}
+	console.log(clinicDatas);
 
 	// map.svgをコードで取得
 	try{
 		const responsSvg = await fetch('../clinic-master/img/map.svg');
+		if (!responsSvg.ok) {
+            throw new Error(`SVGが見つからないよ: ${responsSvg.status}`);
+        }
 		mapSvgCode = await responsSvg.text();
 	}catch(e){
 		console.error(e);
@@ -32,9 +39,39 @@ export async function createMaster(){
 		$map.html(`<div class="clinic__mapimg">${mapSvgCode}</div>`)
 	}
 
-	// function clinicAccordion(){
-	// 	const $list = $('.clinic__accordion');
-	// 	$list.html('');
-	// }
+	function clinicAccordion(){
+		const $accordion = $('.clinic__accordion');
+		let createHtml = [];
+
+		for(let i = 0; i < clinicDatas.length; i++){
+			const area = clinicDatas[i].area;
+			createHtml.push(`<details class="accordion">
+								<summary class="accordion__area">${area}</summary>`);
+
+			const clinicData = clinicDatas[i].clinics;
+
+			for(let j = 0; j < clinicData.length; j++){
+				const clinicName = clinicData[j].clinic_name;
+				const clinicHoursStart = clinicData[j].hours.business_start;
+				const clinicHoursEnd = clinicData[j].hours.business_end;
+				const clinicAddress = clinicData[j].address;
+				const clinicAccess = clinicData[j].full;
+
+				createHtml.push(`
+					<div class="inner">
+						<p class="inner__name">${clinicName}</p>
+						<p class="inner__hours"><span class="inner__ttl">診療時間：</span><span>${clinicHoursStart}〜${clinicHoursEnd}</span></p>
+						<p class="inner__address"><span class="inner__ttl">住所：</span><span>${clinicAddress}</span></p>
+						<p class="inner__access"><span class="inner__ttl">アクセス：</span><span>${clinicAccess}</span></p>
+					</div>
+				`)
+			}
+
+			createHtml.push('</details>');			
+		}
+
+		$accordion.html(createHtml.join(''));
+		
+	}
 
 }
